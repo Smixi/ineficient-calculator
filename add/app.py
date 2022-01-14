c9067703bb94f584dbcdfaeda0334b90568246ac
Sami Tahri <sismixx@hotmail.fr>
2022-01-14 03:23:11 +0100

    add jaeger tracing

--- add/app.py
+++ add/app.py
@@ -4,6 +4,31 @@ from flask import Flask, request
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
 from opentelemetry.instrumentation.requests import RequestsInstrumentor
 from os import environ
+from opentelemetry import trace
+from opentelemetry.exporter.jaeger.thrift import JaegerExporter
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+
+JAEGER_PORT = int(environ.get('TRACER_PORT', 6831))
+JAEGER_HOST = environ.get('TRACER_HOST', 'localhost')
+trace.set_tracer_provider(
+TracerProvider(
+        resource=Resource.create({SERVICE_NAME: "add-ms"})
+    )
+)
+tracer = trace.get_tracer(__name__)
+
+# create a JaegerExporter
+jaeger_exporter = JaegerExporter(
+    # configure agent
+    agent_host_name=JAEGER_HOST,
+    agent_port=JAEGER_PORT
+)
+# Create a BatchSpanProcessor and add the exporter to it
+span_processor = BatchSpanProcessor(jaeger_exporter)
+# add to the tracer
+trace.get_tracer_provider().add_span_processor(span_processor)
 
 app = Flask(__name__)
 FlaskInstrumentor().instrument_app(app)
--- parse/app.py
+++ parse/app.py
@@ -1,5 +1,3 @@
-from inspect import isclass
-import json
 import requests
 import ast
 from ast import BinOp, Constant, Add, Mult, Sub, Div
@@ -7,12 +5,41 @@ from flask import Flask, request
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
 from opentelemetry.instrumentation.requests import RequestsInstrumentor
 from os import environ
+from opentelemetry import trace
+from opentelemetry.exporter.jaeger.thrift import JaegerExporter
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
 
+
+JAEGER_PORT = int(environ.get('TRACER_PORT', 6831))
+JAEGER_HOST = environ.get('TRACER_HOST', 'localhost')
+trace.set_tracer_provider(
+TracerProvider(
+        resource=Resource.create({SERVICE_NAME: "parse-ms"})
+    )
+)
+tracer = trace.get_tracer(__name__)
+
+# create a JaegerExporter
+jaeger_exporter = JaegerExporter(
+    # configure agent
+    agent_host_name=JAEGER_HOST,
+    agent_port=JAEGER_PORT
+)
+
+# Create a BatchSpanProcessor and add the exporter to it
+span_processor = BatchSpanProcessor(jaeger_exporter)
+# add to the tracer
+trace.get_tracer_provider().add_span_processor(span_processor)
+
+
+MS_EVAL = environ.get('MS_EVAL')
 app = Flask(__name__)
 FlaskInstrumentor().instrument_app(app)
 RequestsInstrumentor().instrument()
 
-MS_EVAL = environ.get('MS_EVAL')
+
 
 operator_map = {Add: 'add', Sub: 'sub', Mult: 'mult', Div: 'div'}
 
